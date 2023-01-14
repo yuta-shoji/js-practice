9ac238389ce74ab5b13869d95c133f7f3bceb6ba
Yuta Shoji <shoshoshon.music@gmail.com>
2023-01-14 18:11:10 +0900

    add lesson2-array.js

---
+++ src/lesson2-array.js
@@ -0,0 +1,63 @@
+// 1, 2, 3, 4, 5の数値を格納する配列を宣言しなさい
+
+
+
+// a, b, c, d, eの文字列を格納する配列を宣言しなさい
+
+
+
+// 次の配列の一番最初の要素のindexは何ですか？
+
+const fruitsBasket = ["apple", "lemon", "melon", "blueberry", "pineapple"];
+
+
+
+// 配列fruitsBasketの要素数を取得し出力しなさい
+
+
+
+// 配列fruitsBasketの最初の要素を取り出して出力しなさい
+
+
+
+// 配列fruitsBasketの最後の要素を取り出して出力しなさい
+
+
+
+// 配列fruitsBasketの最初に orange を追加しなさい
+
+
+
+// 配列fruitsBasketの最後に kiwi を追加しなさい
+
+
+
+// 次の配列の最初の要素を削除し、その配列を出力しなさい
+
+const cars = ["camry", "surf", "vezel", "landCruiser", "rumion"];
+
+
+
+// 配列carsの最後の要素を削除し出力しなさい
+
+
+
+// 次の配列の要素のうち、先頭から3つ目までの3つのスイーツを抽出した新たな配列を宣言し、出力しなさい
+
+const sweets = ["cake", "pudding", "iceCream", "parfait", "halohalo"];
+
+
+
+// 配列sweetsのうち、最後から2つ目までの2つのスイーツを抽出した新たな配列を宣言し、出力しなさい
+
+
+
+// 与えられた配列の最初の要素を返す関数 getFirst を宣言して実行しなさい
+
+
+
+// 与えられた配列の最後の要素を返す関数 getLast を宣言して実行しなさい
+
+
+
+// 与えられた配列の最後に jelly の文字列を追加して返す関数 push を宣言して実行しなさい
